fbc056f25b9aa0b650873215853913c387423a0c
sanjay <sanjaysunil34@gmail.com>
2022-11-23 16:43:32 +0530

    cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors({ credentials: true, origin: `${process.env.ORIGIN}` }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', true);
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
